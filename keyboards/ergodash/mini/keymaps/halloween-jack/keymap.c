ca18ea65201b1890e03997fa925b9f71e32d50b8
halloween-jack <halloween.jack.dev@gmail.com>
2018-11-18 13:13:44 +0900

    update keymap

--- keyboards/ergodash/mini/keymaps/halloween-jack/keymap.c
+++ keyboards/ergodash/mini/keymaps/halloween-jack/keymap.c
@@ -32,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------|------|
    * | MOUSE| Ctrl |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  "   | Enter|
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------|------|
-   * | LOWER| Shift|   Z  |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |   /  | Shift| RAISE|
+   * |      | Shift|   Z  |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |   /  | Shift| RAISE|
    * |--------------------+------+------+------+------+------+------+------+------+------+------+------+-------------|------|
-   * |ADJUST|      |      |  ALt ||||||||  GUI | Space|      ||||||||      | Bksp | LOWER||||||||  ALt | Down |  Up  | Right|
+   * |ADJUST| LOWER|      |  ALt ||||||||  GUI | Space|      ||||||||      | Bksp | LOWER||||||||  ALt | Down |  Up  | Right|
    * ,---------------------------------------------------------------------------------------------------------------.------.
    */
   [_QWERTY] = LAYOUT( \
      KC_ESC,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
-     LOWER,   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
+     MOUSE,   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
      _______, KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, RAISE,   \
-     ADJUST,  MOUSE  ,  _______, KC_LALT,          LNG_TOG, KC_SPC, _______,       _______,KC_BSPC, LOWER,            KC_RALT, KC_DOWN, KC_UP,   KC_RGHT  \
+     ADJUST,  LOWER  ,  _______, KC_LALT,          LNG_TOG, KC_SPC, _______,       _______,KC_BSPC, LOWER,            KC_RALT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
